656aedb6a7c1a9aa29d4636fad4c0b08a9603d0e
Your Name <you@example.com>
2018-03-13 20:23:20 +0200

    first step in file validation

--- main.c
+++ main.c
@@ -15,6 +15,7 @@
 #include "libft.h"
 #include "mlx.h"
 #include "get_next_line.h"
+#include "fdf.h"
 
 int 	exit_key(int key, void *param)
 {
@@ -41,27 +42,30 @@ int 	main(int argc, char **argv)
 			ft_strdel(&str);
 		}
 		void *ret;
-		if (!(ret = mlx_init()))
-		{
-			ft_putstr("Connection failed\n");
-			return (1);
-		}
-		void *window = mlx_new_window(ret, 1000, 1000, "test");
-		if (!window)
-		{
-			ft_putstr("Window creation failed");
-			return (1);
-		}
-		void *image = mlx_new_image(ret, 100, 100);
-		if (!image)
-		{
-			ft_putstr("Image creation failed");
-			return (1);
-		}
-		mlx_put_image_to_window(ret, window, image, 10, 10);
-		mlx_key_hook(window, exit_key, (void*)0);
-		mlx_loop(ret);
-
+//		if (!(ret = mlx_init()))
+//		{
+//			ft_putstr("Connection failed\n");
+//			return (1);
+//		}
+//		void *window = mlx_new_window(ret, 1000, 1000, "test");
+//		if (!window)
+//		{
+//			ft_putstr("Window creation failed");
+//			return (1);
+//		}
+//		void *image = mlx_new_image(ret, 100, 100);
+//		if (!image)
+//		{
+//			ft_putstr("Image creation failed");
+//			return (1);
+//		}
+	//	mlx_put_image_to_window(ret, window, image, 10, 10);
+		//mlx_key_hook(window, exit_key, (void*)0);
+		//mlx_loop(ret);
+		ft_putendl("\n10-70.fdf");
+		int fd1 = open("test_maps/10-70.fdf", O_RDONLY);
+		if (fd1 >= 0)
+			read_map(fd1);
 	}
 	return (0);
 }
--- read_map.c
+++ read_map.c
@@ -10,4 +10,80 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include ""
+#include "libft.h"
+#include "get_next_line.h"
+#include <fcntl.h>
+
+void	print_lst(t_list *list)
+{
+	while (list)
+	{
+		ft_putendl_fd((char*)list->content, 0);
+		list = list->next;
+	}
+}
+
+int 	is_num(char *str)
+{
+	size_t i;
+	int counter;
+	size_t len;
+
+	i = 0;
+	counter = 0;
+	len = ft_strlen(str);
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]) && str[i] != ' ' && str[i] != '-')
+			return (0);
+		if (i + 1 < len && str[i] == '-' && !ft_isdigit(str[i + 1]))
+			return(0);
+		if (i > 0 && str[i] == '-' && str[i - 1] != ' ')
+			return(0);
+		if (i > 0 && str[i] == ' ' && ft_isdigit(str[i - 1]))
+			counter++;
+		i++;
+	}
+	if (ft_isdigit(str[i - 1]))
+		counter++;
+	return (counter);
+}
+
+void 	del(void *cont, size_t size)
+{
+	if (size)
+		//free(&cont);
+		ft_putendl("Del list");
+}
+
+int 	read_map(int fd)
+{
+	t_list	*list;
+	t_list	*node;
+	char	*buf;;
+	int num;
+	int first;
+	list = NULL;
+
+	num = -1;
+	first = -1;
+	while (get_next_line(fd, &buf) == 1)
+	{
+		node = ft_lstnew(ft_strtrim(buf), ft_strlen(buf));
+		if (first == -1 && num != -1)
+			first = num;
+		if (!(num = is_num(buf)) || (first != -1 && first != num))
+		{
+			if (buf)
+				ft_strdel(&buf);
+			if (list)
+				ft_lstdel(&list, &del);
+			return (0);
+		}
+		else
+			ft_lstadd(&list, node);
+		ft_strdel(&buf);
+	}
+	print_lst(list);
+	return (1);
+}
