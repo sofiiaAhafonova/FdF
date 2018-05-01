f0e174f57ecca006214ade595294a146c4469f14
sahafono <amita051197@gmail.com>
2018-05-01 14:08:56 +0300

    norme init_map.c

--- init_map.c
+++ init_map.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   zoom_map.c                                         :+:      :+:    :+:   */
+/*   init_map.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sahafono <sahafono@student.unit.ua>        +#+  +:+       +#+        */
+/*   By: sahafono <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/04/30 14:48:00 by sahafono          #+#    #+#             */
-/*   Updated: 2018/04/30 14:48:00 by sahafono         ###   ########.fr       */
+/*   Created: 2018/05/01 14:04:48 by sahafono          #+#    #+#             */
+/*   Updated: 2018/05/01 14:04:50 by sahafono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-int start_zoom(t_map *map)
+int		start_zoom(t_map *map)
 {
 	if (map->row <= 10 && map->col <= 30)
 		return (50);
@@ -33,24 +33,27 @@ int start_zoom(t_map *map)
 	return (2);
 }
 
-int initialize_map(t_map *map)
+int		initialize_map(t_map *map)
 {
+	int i;
+
 	if (!(map->mlx_ptr = mlx_init()))
 		return (print_error("mlx init error"));
-	if (!(map->window = mlx_new_window(map->mlx_ptr, SCREEN_WIDTH, SCREEN_HEIGHT, "FdF")))
+	if (!(map->window = mlx_new_window(map->mlx_ptr,
+		SCREEN_WIDTH, SCREEN_HEIGHT, "FdF")))
 		return (print_error("window creation error"));
 	map->zoom = start_zoom(map);
 	zoom_map(map);
-	map->offset = (t_dot**)malloc(sizeof(t_dot *) * map->row);
-	int i = -1;
+	map->offset = (t_dot**)malloc(sizeof(t_dot*) * map->row);
+	i = -1;
 	while (++i < map->row)
-		map->offset[i] = malloc(sizeof(t_dot)*map->col);
+		map->offset[i] = malloc(sizeof(t_dot) * map->col);
 	map->offset_x = SCREEN_WIDTH / 2;
-	map->offset_y =  SCREEN_HEIGHT / 2;
-	map->wz = 0 *  DEGREE;
-	map->wy = 2 *  DEGREE;
-	map->wx = 2 *  DEGREE;
+	map->offset_y = SCREEN_HEIGHT / 2;
+	map->wz = 0 * DEGREE;
+	map->wy = 2 * DEGREE;
+	map->wx = 2 * DEGREE;
 	map->show_legend = 1;
 	map->scale_z = 1;
-    return (0);
+	return (0);
 }
